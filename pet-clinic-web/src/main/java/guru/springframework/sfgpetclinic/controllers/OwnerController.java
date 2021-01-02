40cbcbcc16a6e9cdf5150d20fe2da726ed6917d5
ybekardev <y_bekar@hotmail.com>
2021-01-01 22:47:37 -0600

    added vet controller on index page. Closes #13

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "index", "index.html"})
+    private String listOwners(){
+        return "owners/index";
+    }
+
+}
