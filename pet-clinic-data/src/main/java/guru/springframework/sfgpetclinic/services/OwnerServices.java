33d7a242118abd0c1774a00f0482c8b5913e16b3
ybekardev <y_bekar@hotmail.com>
2021-01-01 20:23:13 -0600

    added service interface. Closes #5

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerServices.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerServices.java
@@ -4,7 +4,6 @@ import guru.springframework.sfgpetclinic.model.Owner;
 import java.util.Set;
 
 public interface OwnerServices {
-
     Owner findByLastName(String lastName);
     Owner findById(Long id);
     Owner save(Owner owner);
