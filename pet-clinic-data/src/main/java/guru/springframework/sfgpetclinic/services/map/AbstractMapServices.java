183db0143e2befa773ddad0c919659414abaa32c
ybekardev <y_bekar@hotmail.com>
2021-01-03 19:34:08 -0600

    added vets list. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapServices.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapServices.java
@@ -1,12 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-public class AbstractMapServices<T, ID>{
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public class AbstractMapServices<T extends BaseEntity, ID extends Long>{
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +16,15 @@ public class AbstractMapServices<T, ID>{
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+        if(object != null){
+            if(object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else{
+            throw new RuntimeException("Object has a null ID");
+        }
         return object;
     }
 
@@ -30,4 +37,14 @@ public class AbstractMapServices<T, ID>{
     }
 
 
+    private Long getNextId(){
+
+        Long nextId =  null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
+    }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -25,7 +25,7 @@ public class PetServiceMap extends AbstractMapServices<Pet, Long> implements Pet
 
     @Override
     public Pet save(Pet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
