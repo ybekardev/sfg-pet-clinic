1387c4edd656ac87546b40f36ac83cc1519234e8
ybekardev <y_bekar@hotmail.com>
2021-01-03 19:41:14 -0600

    added ID. Closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapServices.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapServices.java
@@ -4,7 +4,7 @@ import guru.springframework.sfgpetclinic.model.BaseEntity;
 
 import java.util.*;
 
-public class AbstractMapServices<T extends BaseEntity, ID extends Long>{
+public abstract class AbstractMapServices<T extends BaseEntity, ID extends Long>{
 
     protected Map<Long, T> map = new HashMap<>();
 
