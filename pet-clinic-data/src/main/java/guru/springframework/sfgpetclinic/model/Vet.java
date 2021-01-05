b9c006481d0a38f8eac008841fedc4f385ca6cf6
ybekardev <y_bekar@hotmail.com>
2021-01-04 21:30:28 -0600

    added vet

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,7 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
+    private Set<Speciality> speciality;
 
+    public Set<Speciality> getSpeciality() {
+        return speciality;
+    }
 
+    public void setSpeciality(Set<Speciality> speciality) {
+        this.speciality = speciality;
+    }
 }
